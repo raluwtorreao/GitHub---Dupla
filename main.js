6a181fbc7a801e016400d8e3d8fa12ef0f6dc8ee
raluwtorreao <torreaobarbara@gmail.com>
2023-05-07 19:38:01 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+console.log("Oi, Renan <3")
+window.onload = function() {
+    var imagem = document.createElement("img");
+    imagem.setAttribute("src", "Imagens - trabalho/img01.png");
+    document.getElementById("oioi").appendChild(imagem);
+}
